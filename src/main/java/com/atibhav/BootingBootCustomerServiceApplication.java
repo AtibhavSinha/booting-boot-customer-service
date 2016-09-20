0fdf9e74600723b9ed96d380de9b44838dd9751f
atsinha <atsinha@deloitte.com>
2016-09-20 15:22:32 +0530

    Second commit

--- src/main/java/com/atibhav/BootingBootCustomerServiceApplication.java
+++ src/main/java/com/atibhav/BootingBootCustomerServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootingBootCustomerServiceApplication {
 
 	public static void main(String[] args) {
+		//Start the application
 		SpringApplication.run(BootingBootCustomerServiceApplication.class, args);
 	}
 }
